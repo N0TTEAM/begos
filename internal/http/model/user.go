1bff81ea78230c9290712867de50d1894c6beb72
forcecode <n0tteam060@gmail.com>
2025-01-29 00:29:02 -0800

    add validation

--- internal/http/model/user.go
+++ internal/http/model/user.go
@@ -4,8 +4,8 @@ import "time"
 
 type User struct {
 	Id        uint
-	Username  string
-	Email     string
+	Username  string `validate:"required"`
+	Email     string `validate:"required"`
 	Password  string
 	CreatedAt time.Time
 	UpdatedAt time.Time
