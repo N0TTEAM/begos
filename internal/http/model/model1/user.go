1a67259a778d85879fe7fbf4930fb2d003701e65
forcecode <n0tteam060@gmail.com>
2025-02-04 22:04:48 -0800

    update gitignore

--- internal/http/model/model1/user.go
+++ internal/http/model/model1/user.go
@@ -6,7 +6,7 @@ type User struct {
 	ID        uint   `gorm:"primaryKey"`
 	Username  string `validate:"required"`
 	Email     string `validate:"required"`
-	Password  string `gorm:"type:varchar(255);not null"`
+	GoogleID  string `gorm:"type:varchar(255);unique;not null"`
 	Role      string `gorm:"type:enum('admin','customer');default:'customer'"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
